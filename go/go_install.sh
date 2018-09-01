82de1e694e3ea88ecfc1346663b1b4c35653c784
Cory Smith <smith.cory@gmail.com>
2018-09-01 10:13:26 -0700

    delete old version (#4)

--- go/go_install.sh
+++ go/go_install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 GO_TGZ="go1.11.linux-amd64.tar.gz"
 wget "https://dl.google.com/go/${GO_TGZ}"
+sudo rm -rf /usr/local/go # remove existing version
 sudo tar -C /usr/local -xzf "${GO_TGZ}"
 sudo grep -q -F 'export PATH=$PATH:/usr/local/go/bin' "${HOME}/.bashrc" || echo 'export PATH=$PATH:/usr/local/go/bin' >> "${HOME}/.bashrc"
 sudo grep -q -F 'export PATH=$PATH:${HOME}/go/bin' "${HOME}/.bashrc" || echo 'export PATH=$PATH:${HOME}/go/bin' >> "${HOME}/.bashrc"
