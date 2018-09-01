2391fb975d44dff6210fcd7ce80e47496f6df818
Cory Smith <smith.cory@gmail.com>
2018-09-01 09:38:49 -0700

    update to go1.11

--- go/go_install.sh
+++ go/go_install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-GO_TGZ="go1.10.3.linux-amd64.tar.gz"
+GO_TGZ="go1.11.linux-amd64.tar.gz"
 wget "https://dl.google.com/go/${GO_TGZ}"
 sudo tar -C /usr/local -xzf "${GO_TGZ}"
 sudo grep -q -F 'export PATH=$PATH:/usr/local/go/bin' "${HOME}/.bashrc" || echo 'export PATH=$PATH:/usr/local/go/bin' >> "${HOME}/.bashrc"
+sudo grep -q -F 'export PATH=$PATH:${HOME}/go/bin' "${HOME}/.bashrc" || echo 'export PATH=$PATH:${HOME}/go/bin' >> "${HOME}/.bashrc"
 source "${HOME}/.bashrc"
