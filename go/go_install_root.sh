d97533485b37f1fcef5b412c1cbd128a5ab74a18
Cory Smith <smith.cory@gmail.com>
2019-01-27 20:53:07 -0800

    added go install for root

---
+++ go/go_install_root.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+GO_TGZ="go1.11.5.linux-amd64.tar.gz"
+wget "https://dl.google.com/go/${GO_TGZ}"
+rm -rf /usr/local/go # remove existing version
+tar -C /usr/local -xzf "${GO_TGZ}"
+grep -q -F 'export PATH=$PATH:/usr/local/go/bin' "${HOME}/.bashrc" || echo 'export PATH=$PATH:/usr/local/go/bin' >> "${HOME}/.bashrc"
+grep -q -F 'export PATH=$PATH:${HOME}/go/bin' "${HOME}/.bashrc" || echo 'export PATH=$PATH:${HOME}/go/bin' >> "${HOME}/.bashrc"
+source "${HOME}/.bashrc"
