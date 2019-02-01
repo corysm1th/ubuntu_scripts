b5884fdb649c2e3ebe8c4042e56ee1d920c2c465
Cory Smith <smith.cory@gmail.com>
2019-02-01 15:44:03 -0800

    Added explicit GOPATH for scripting

--- go/go_install.sh
+++ go/go_install.sh
@@ -5,4 +5,5 @@ sudo rm -rf /usr/local/go # remove existing version
 sudo tar -C /usr/local -xzf "${GO_TGZ}"
 sudo grep -q -F 'export PATH=$PATH:/usr/local/go/bin' "${HOME}/.bashrc" || echo 'export PATH=$PATH:/usr/local/go/bin' >> "${HOME}/.bashrc"
 sudo grep -q -F 'export PATH=$PATH:${HOME}/go/bin' "${HOME}/.bashrc" || echo 'export PATH=$PATH:${HOME}/go/bin' >> "${HOME}/.bashrc"
+sudo grep -q -F 'export GOPATH=${HOME}/go' "${HOME}/.bashrc" || echo 'export GOPATH=${HOME}/go' >> "${HOME}/.bashrc"
 source "${HOME}/.bashrc"
