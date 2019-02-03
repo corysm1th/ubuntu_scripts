315062c202447392f9b2f88dd55b758742b42fd1
Cory Smith <smith.cory@gmail.com>
2019-02-02 16:12:54 -0800

    execute with bash

--- go/go_install.sh
+++ go/go_install.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 GO_TGZ="go1.11.5.linux-amd64.tar.gz"
-wget "https://dl.google.com/go/${GO_TGZ}"
+wget -q "https://dl.google.com/go/${GO_TGZ}"
 sudo rm -rf /usr/local/go # remove existing version
 sudo tar -C /usr/local -xzf "${GO_TGZ}"
 sudo grep -q -F 'export PATH=$PATH:/usr/local/go/bin' "${HOME}/.bashrc" || echo 'export PATH=$PATH:/usr/local/go/bin' >> "${HOME}/.bashrc"
